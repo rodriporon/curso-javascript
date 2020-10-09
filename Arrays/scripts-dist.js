bd530bc30e8fd02fe06d1c87463f34ff979d9a5d
rodriporon <3065713820401@ingenieria.usac.edu.gt>
2020-10-08 21:34:12 -0600

    Arrays

---
+++ Arrays/scripts-dist.js
@@ -0,0 +1,18 @@
+
+let numeros = [5,6,3,2];
+//console.log(numeros);
+
+//numeros.reverse();
+//console.log(numeros)
+
+let names = ['Rodri', 'Diego', 'Ali'];
+
+/* for (let name of names){
+    console.log(name);
+}
+
+for (let index in names) {
+    console.log(index);
+} */
+
+
