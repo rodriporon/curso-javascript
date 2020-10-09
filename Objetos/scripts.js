692fdf41ac7f650518fad3196cdade1382b3afa3
rodriporon <3065713820401@ingenieria.usac.edu.gt>
2020-10-08 21:56:22 -0600

    Objetos

---
+++ Objetos/scripts.js
@@ -0,0 +1,24 @@
+
+const person = {
+    name: 'Rodri',
+    age: 21,
+    sons: ['Kimo', 'Scrapy', "Boxer"]
+
+}
+
+/* console.log(person.name);
+console.log(person['name']); */
+
+/* for (const key in person) {
+    console.log(key);
+} */
+
+/* for (const inety in person) {
+    console.log(person[inety]);
+} */
+
+/* for (const son of person.sons) {
+    console.log(son);
+} */
+
+//console.log(`Hola ${person.name}, tienes ${person.age} años y tus hijos son: ${person.sons.join(', ')}`)
